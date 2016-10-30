2e0fd4453b9e1a2f91b01bb391ec38c99355a2db
Kenji Hata <a.kenjihata@gmail.com>
2016-10-30 12:39:40 -0700

    changing for initial ZF test

--- lib/fast_rcnn/config.py
+++ lib/fast_rcnn/config.py
@@ -44,7 +44,7 @@ __C.TRAIN.MAX_SIZE = 1000
 __C.TRAIN.IMS_PER_BATCH = 2 
 
 # Minibatch size (number of regions of interest [ROIs])
-__C.TRAIN.BATCH_SIZE = 32 
+__C.TRAIN.BATCH_SIZE = 128 
 
 # Fraction of minibatch that is labeled foreground (i.e. class > 0)
 __C.TRAIN.FG_FRACTION = 0.25
